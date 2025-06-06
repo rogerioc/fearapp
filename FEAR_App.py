7781a650997508f4985247983e5ddf6c293ab7ae
Rogério C. Santos <rogerio.celestino@gmail.com>
2025-06-06 16:10:57 -0300

    Improve structure and add gitignore

--- FEAR_App.py
+++ FEAR_App.py
@@ -1,62 +1,23 @@
 # App web para responder perguntas sobre filmes de terror usando ChatGPT e langchain.
 # Imports
 import streamlit as st
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain_core.messages import BaseMessage
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from pydantic import BaseModel, Field
-
+import QuestionAnswerChain as qa
+from QuestionAnswerChain import InMemoryHistory
 # Carrega o arquivo de variáveis de ambiente
 # Configure seu .env com as variáveis necessárias
 # OPENAI_API_KEY
 # Usar se for utilizar variáveis de ambiente
 # load_dotenv()
 
-
-# Template para a conversa
 template = """Esta é uma conversa entre um cinéfilo e um especialista em filmes de terror. 
                 Você é o especialista em filmes de terror, conhece bem os filmes clássicos e novos de terror e deve responder com a maior precisão possível.
 
-Current conversation:
-{history}
-Human: {input}
-TerrorSpecialist:"""
+    Current conversation:
+    {history}
+    Human: {input}
+    TerrorSpecialist:"""
 
-question_prompt = PromptTemplate(input_variables = ["history", "input"], template = template)
-
-# Classe para armazenar o histórico de mensagens em memória
-class InMemoryHistory(BaseChatMessageHistory, BaseModel):
-    messages: list[BaseMessage] = Field(default_factory=list)
-    def add_messages(self, messages: list[BaseMessage]) -> None:        
-        self.messages.extend(messages)
-
-    def clear(self) -> None:
-        self.messages = []
-
-store = {}
-
-# Função para obter o histórico de mensagens por ID de sessão
-def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = InMemoryHistory()
-    return store[session_id]
-
-def config(chatGpt):
-    chain = question_prompt | chatGpt
-    ## CobnversationChain será descontinuado em breve, então comecei a usar o RunnableWithMessageHistory para entender as diferenças
-    return RunnableWithMessageHistory(chain, get_by_session_id,
-                                          input_messages_key="input",
-                                          history_messages_key="history")
-
-def get_response(question, conversation):
-    prompt = { "ability": "filmes", "input": question}        
-    resultado = conversation.invoke(prompt, config={"configurable": {"session_id": "1"}})
-    print(store)
-    return "Especialista: " + resultado.content
-
+questionAnswer = qa.QuestionAnswerChain(template = template, inMemoryHistory = InMemoryHistory())
 ########## App Web ##########
 
 # Configuração da página do Streamlit
@@ -66,10 +27,6 @@ openai_api_key = st.sidebar.text_input("Adicione sua OpenAI API Key", type = "pa
 st.sidebar.markdown("""
 https://platform.openai.com/
 """)                    
-# Configuração do modelo ChatGPT
-def configModel(openai_api_key):
-    chatGpt = ChatOpenAI(temperature = 0.5, openai_api_key=openai_api_key)
-    return config(chatGpt)
     
 # Barra Lateral com instruções
 st.sidebar.title("Instruções")
@@ -100,13 +57,13 @@ if st.button("Analisar"):
         st.stop()
     else:
         # Configura o modelo ChatGPT com a chave de API fornecida
-        conversation = configModel(openai_api_key)
+        questionAnswer.settingKey(openai_api_key)
     # Se temos o código da ação (ticker)
     if question:
 
         # Inicia o processamento
         with st.spinner("Buscando a resposta. Aguarde..."):                                            
-            resposta = get_response(question, conversation)
+            resposta =  questionAnswer.get_response(question)
             # Imprime a resposta
             st.markdown(resposta)            
             
---
+++ QuestionAnswerChain.py
@@ -0,0 +1,48 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.messages import BaseMessage
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from pydantic import BaseModel, Field
+
+# Classe para armazenar o histórico de mensagens em memória
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    messages: list[BaseMessage] = Field(default_factory=list)
+    def add_messages(self, messages: list[BaseMessage]) -> None:        
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+
+class QuestionAnswerChain:
+    # Template para a conversa
+       
+    def __init__(self,template, inMemoryHistory):
+        self.template = template
+        self.question_prompt = PromptTemplate(input_variables = ["history", "input"], template = template)
+        self.store = {}
+        self.inMemoryHistory = inMemoryHistory        
+
+    def settingKey(self,openai_api_key):
+        self.chatGpt = ChatOpenAI(temperature = 0.5, openai_api_key=openai_api_key)
+        self.__configChain()
+    
+    # Função para obter o histórico de mensagens por ID de sessão
+    def __get_by_session_id(self,session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = self.inMemoryHistory
+        return self.store[session_id]
+
+    def __configChain(self):
+        chain = self.question_prompt | self.chatGpt
+        ## CobnversationChain será descontinuado em breve, então comecei a usar o RunnableWithMessageHistory para entender as diferenças
+        self.conversation = RunnableWithMessageHistory(chain, self.__get_by_session_id,
+                                            input_messages_key="input",
+                                            history_messages_key="history")
+
+    def get_response(self,question):
+        prompt = { "ability": "filmes", "input": question}        
+        resultado = self.conversation.invoke(prompt, config={"configurable": {"session_id": "1"}})
+        print(self.store)
+        return "Especialista: " + resultado.content
